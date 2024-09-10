4b8851ec89f6d2b6fc488688db274bd7dfc49c9d
Yan Basílio <69776644+YanFellippe@users.noreply.github.com>
2024-09-10 09:32:58 -0300

    Add files via upload

--- js/55/55.js
+++ js/55/55.js
@@ -2,8 +2,8 @@ function mudaTxt(){
     const texto = document.querySelector('.texto');
     const btn = document.querySelector('.btn');
 
-    texto.innerText = 'Você clicou no botão!';
-    btn.innerText = 'Obrigado por Clicar';
+    texto.textContent = 'Você clicou no botão!';
+    btn.textContent = 'Obrigado por Clicar';
 }
 
 document.querySelector('#mudaTxt').onclick = mudaTxt;

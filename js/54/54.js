2785b5e8834b912097d17f7b9f3a0cc67d1d39e2
Yan Basílio <69776644+YanFellippe@users.noreply.github.com>
2024-09-10 08:42:17 -0300

    Add files via upload

--- js/54/54.js
+++ js/54/54.js
@@ -1,7 +1,9 @@
-    const div = document.querySelector('div');
-    const button = document.querySelector('button');
-
-    button.addEventListener("click", function mudaCSS(){
-        div.style.backgroundColor = 'gray';
-        div.style.color = 'lightblue';
-    });
+    
+    //função que altera o css no click
+    function mudaCSS(){
+        const caixa = document.querySelector('.caixa');
+        //adiciona a classe nova 'caixa-ativa' 
+        caixa.classList.add('caixa-ativa');
+    }
+    //chama a função que altera o css ao clicar no botão
+    document.querySelector('#mudaCSS').onclick = mudaCSS;

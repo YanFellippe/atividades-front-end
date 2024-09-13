c18ab86ee8f84499b5066be491a86dbdb3b90222
Yan Basílio <69776644+YanFellippe@users.noreply.github.com>
2024-09-13 10:41:31 -0300

    Add files via upload

---
+++ js/62/62.js
@@ -0,0 +1,14 @@
+let btn = document.getElementById('btn');
+btn.addEventListener('click', cadastro);
+
+function cadastro(){
+    let nome =document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let cpf = document.getElementById('cpf').value;
+    let endereco = document.getElementById('endereco').value;
+    let complemento = document.getElementById('complemento').value
+    let uf = document.getElementById('uf').value;
+    let resultado = document.getElementById('resultado');
+
+    resultado.innerHTML = `Bem vindo <strong>${nome}</strong>. Email: <strong>${email}</strong> CPF:<strong>${cpf}</strong> Endereco: ${endereco} - Complemento: <strong>${complemento}</strong> - UF: <strong>${uf}</strong>`;
+}

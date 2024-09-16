b2cdf76594e4f62f38df41da57a8f0c4fdb8520b
Yan Basílio <69776644+YanFellippe@users.noreply.github.com>
2024-09-16 11:19:22 -0300

    Add files via upload

---
+++ js/65/65.js
@@ -0,0 +1,5 @@
+function mudaLista(){
+        document.getElementById('top1').innerHTML = 'The Witcher 3';
+        document.getElementById('top2').innerHTML = 'GTA VI';
+        document.getElementById('top3').innerHTML = 'Call of Duty: Black Ops 2';
+}

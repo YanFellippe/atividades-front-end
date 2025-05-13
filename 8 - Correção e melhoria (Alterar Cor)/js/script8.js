873c3ea225f520afb977d2b357297f8edfef1293
Yan Basílio <69776644+YanFellippe@users.noreply.github.com>
2025-05-13 15:38:47 -0300

    Add files via upload

---
+++ 8 - Correção e melhoria (Alterar Cor)/js/script8.js
@@ -0,0 +1,5 @@
+function mudarCor() {
+  // Função para alterar a cor do elemento selecionado
+  const cor = document.getElementById('corSelecionada').value;
+  document.getElementById('texto').style.color = cor; 
+}
